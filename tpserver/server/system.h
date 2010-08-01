80d493ac28e3ed9dc328580cdfb4d68ed74d4abd
unknown <okami@.(none)>
2010-08-02 01:44:18 +0200

    system.h upgrade
      * instead of using the non-portable stdint.h we use boost/cstdint.hpp
      * common typedefs are brought out of the boost namespace

--- tpserver/server/system.h
+++ tpserver/server/system.h
@@ -27,8 +27,6 @@
 
 #else
 
-#  include <stdint.h>
-
 #  ifdef HAVE_CONFIG_H
 #    include "config.h"
 #  else
@@ -36,5 +34,17 @@
 #  endif
 
 #endif
+
+#include <boost/cstdint.hpp>
+
+typedef boost::uint8_t uint8_t;
+typedef boost::uint16_t uint16_t;
+typedef boost::uint32_t uint32_t;
+typedef boost::uint64_t uint64_t;
+typedef boost::int8_t int8_t;
+typedef boost::int16_t int16_t;
+typedef boost::int32_t int32_t;
+typedef boost::int64_t int64_t;
+
 
 #endif // SYSTEM_H
