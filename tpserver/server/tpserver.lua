877f1e82783c4400446023caef5d628eed96618a
Kornel Kisielewicz <epyon@sakura.(none)>
2010-08-10 20:43:15 +0200

    Formatting fix

--- tpserver/server/tpserver.lua
+++ tpserver/server/tpserver.lua
@@ -1,23 +1,23 @@
 -- A solution contains projects, and defines the available configurations
 project "tpserver"
-	kind "ConsoleApp"
-    language "C++"
-    files { "**.h", "**.cpp" }
-	flags { "ExtraWarnings", "NoPCH" }
-	includedirs { "../../", ".", "/opt/local/include/" }
-	targetname "http-proxy"
+  kind "ConsoleApp"
+  language "C++"
+  files { "**.h", "**.cpp" }
+  flags { "ExtraWarnings", "NoPCH" }
+  includedirs { "../../", ".", "/opt/local/include/" }
+  targetname "http-proxy"
   links { "boost_system", "boost_thread-mt" }
   libdirs { "/opt/local/lib/" }
- 
-    configuration "debug"
-		defines { "DEBUG" }
-		flags { "Symbols" }
-  		targetdir "bin"
-		objdir "temp/debug"
 
- 
-    configuration "release"
-		defines { "NDEBUG" }
-		flags { "Optimize" }
-		targetdir "bin"
-		objdir "temp/release"
+  configuration "debug"
+    defines { "DEBUG" }
+    flags { "Symbols" }
+    targetdir "bin"
+    objdir "temp/debug"
+
+
+  configuration "release"
+    defines { "NDEBUG" }
+    flags { "Optimize" }
+    targetdir "bin"
+    objdir "temp/release"
